6f53ee035dcf6d1e4d7204ba49a52b3a3c4c51ec
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2026-02-27 12:52:19 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "192.168.20.26:3000", "0.0.0.0:3000"],
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
